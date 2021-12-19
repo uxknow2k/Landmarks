e0db91cca7b46fe5eda140325d1373bc25c2470b
uxknow2k <EatingMyTree@yandex.ru>
2021-12-19 22:43:25 +0300

    Updated project

---
+++ src/main/java/org/example/dto/LandmarkGetDistanceBetweenPlacesDTO.java
@@ -0,0 +1,35 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class LandmarkGetDistanceBetweenPlacesDTO {
+    private List<Landmark> landmarks;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
+    public static class Landmark {
+        private long id;
+        private String name;
+        private String city;
+        private String landmarkAddress;
+        private String undergrounds;
+        private String landmarkDescription;
+        private String landmarkWebSite;
+        private String landmarkPhone;
+        private String open;
+        private String close;
+        private Boolean available;
+        private Double lat;
+        private Double log;
+        private String image;
+
+    }
+}
--- src/main/java/org/example/manager/LandmarkManager.java
+++ src/main/java/org/example/manager/LandmarkManager.java
@@ -94,8 +94,17 @@ public class LandmarkManager {
                 lat, lon);
     }
 
-    public List<Landmark> getAllInThisRadius(long id, double lat, double lon, int radius) {
-        LandmarkGetByIdResponseDTO landmarkGetByIdResponseDTO = getById(id);
+    public LandmarkGetDistanceBetweenPlacesDTO getFullAllResponseDTO() {
+        final List<LandmarkFullModel> items = template.query(
+                """
+                        SELECT  lat, lon FROM landmarks
+                        VALUES (:lat, :lon)
+                        RETURNING id, name, city, landmark_address, undergrounds, landmark_description, 
+                            landmark_web_site, landmark_phone, image, open, close, CURRENT_TIME BETWEEN open AND close AS available,
+                        WHERE removed = FALSE   
+                        """,
+                landmarkFullMapper
+        );
         LandmarkGetDistanceBetweenPlacesDTO responseDTO = getFullAllResponseDTO();
         List<LandmarkGetDistanceBetweenPlacesDTO.Landmark> result = new ArrayList<>();
         for (LandmarkGetDistanceBetweenPlacesDTO.Landmark landmark : responseDTO.getLandmarks()) {
@@ -108,123 +117,73 @@ public class LandmarkManager {
         }
 
 
-        return result;
-    }
+        public LandmarkGetByIdResponseDTO getById ( long id){
+            try {
+                final LandmarkFullModel item = template.queryForObject(
+                        // language=PostgreSQL
+                        """
+                                SELECT id, name, city, landmark_address, undergrounds,landmark_description, 
+                                landmark_web_site, landmark_phone, open, close, CURRENT_TIME BETWEEN open AND close AS available, lat, lon,  image FROM landmarks
+                                WHERE id = :id AND removed = FALSE   
+                                """,
+                        Map.of(
+                                "id", id
+                        ),
+                        landmarkFullMapper
+                );
+                final LandmarkGetByIdResponseDTO responseDTO = new LandmarkGetByIdResponseDTO(new LandmarkGetByIdResponseDTO.Landmark(
+                        item.getId(),
+                        item.getName(),
+                        item.getCity(),
+                        item.getLandmarkAddress(),
+                        item.getUndergrounds(),
+                        item.getLandmarkDescription(),
+                        item.getLandmarkWebSite(),
+                        item.getLandmarkPhone(),
+                        item.getOpen(),
+                        item.getClose(),
+                        item.getAvailable(),
+                        item.getLon(),
+                        item.getLat(),
+                        item.getImage()
+                ));
 
-    public LandmarkGetByIdResponseDTO getById(long id) {
-        try {
-            final LandmarkFullModel item = template.queryForObject(
-                    // language=PostgreSQL
-                    """
-                            SELECT id, name, city, landmark_address, undergrounds,landmark_description, 
-                            landmark_web_site, landmark_phone, open, close, CURRENT_TIME BETWEEN open AND close AS available, lat, lon,  image FROM landmarks
-                            WHERE id = :id AND removed = FALSE   
-                            """,
-                    Map.of(
-                            "id", id
-                    ),
-                    landmarkFullMapper
-            );
-            final LandmarkGetByIdResponseDTO responseDTO = new LandmarkGetByIdResponseDTO(new LandmarkGetByIdResponseDTO.Landmark(
-                    item.getId(),
-                    item.getName(),
-                    item.getCity(),
-                    item.getLandmarkAddress(),
-                    item.getUndergrounds(),
-                    item.getLandmarkDescription(),
-                    item.getLandmarkWebSite(),
-                    item.getLandmarkPhone(),
-                    item.getOpen(),
-                    item.getClose(),
-                    item.getAvailable(),
-                    item.getLon(),
-                    item.getLat(),
-                    item.getImage()
-            ));
-
-            return responseDTO;
-        } catch (EmptyResultDataAccessException e) {
-            throw new LandmarkNotFoundException(e);
+                return responseDTO;
+            } catch (EmptyResultDataAccessException e) {
+                throw new LandmarkNotFoundException(e);
+            }
         }
-    }
 
-    public LandmarkSaveResponseDTO save(LandmarkSaveRequestDTO requestDTO) {
-        return requestDTO.getId() == 0 ? create(requestDTO) : update(requestDTO);
-    }
+        public LandmarkSaveResponseDTO save (LandmarkSaveRequestDTO requestDTO){
+            return requestDTO.getId() == 0 ? create(requestDTO) : update(requestDTO);
+        }
 
 
-    private LandmarkSaveResponseDTO create(LandmarkSaveRequestDTO requestDTO) {
-        final LandmarkFullModel item = template.queryForObject(
-                // language=PostgreSQL
-                """
-                        INSERT INTO landmarks (name, city, landmark_address, undergrounds, landmark_description, 
-                            landmark_web_site, landmark_phone, image, open, close, lat, lon)  
-                            VALUES (:name, :city, :landmarkAddress, :undergrounds, 
-                            :landmarkDescription, :landmarkWebSite, :landmarkPhone, :image, :open::time with time zone AT TIME ZONE  'UTC',
-                            :close::time with time zone AT TIME ZONE  'UTC', :lat, :lon)
-                        RETURNING id, name, city, landmark_address, undergrounds, landmark_description, 
-                            landmark_web_site, landmark_phone, image, open, close, CURRENT_TIME BETWEEN open AND close AS available, lat, lon
-                        """,
-                Map.ofEntries(
-                        Map.entry("name", requestDTO.getName()),
-                        Map.entry("city", requestDTO.getCity()),
-                        Map.entry("landmarkAddress", requestDTO.getLandmarkAddress()),
-                        Map.entry("undergrounds", requestDTO.getUndergrounds()),
-                        Map.entry("landmarkDescription", requestDTO.getLandmarkDescription()),
-                        Map.entry("landmarkWebSite", requestDTO.getLandmarkWebSite()),
-                        Map.entry("landmarkPhone", requestDTO.getLandmarkPhone()),
-                        Map.entry("image", requestDTO.getImage() == null ? defaultImage : requestDTO.getImage()),
-                        Map.entry("open", requestDTO.getOpen()),
-                        Map.entry("close", requestDTO.getClose()),
-                        Map.entry("lat", requestDTO.getLon()),
-                        Map.entry("lon", requestDTO.getLat())
-                ),
-                landmarkFullMapper
-
-        );
-
-        final LandmarkSaveResponseDTO responseDTO = new LandmarkSaveResponseDTO(new LandmarkSaveResponseDTO.Landmark(
-                item.getId(),
-                item.getName(),
-                item.getCity(),
-                item.getLandmarkAddress(),
-                item.getUndergrounds(),
-                item.getLandmarkDescription(),
-                item.getLandmarkWebSite(),
-                item.getOpen(),
-                item.getClose(),
-                item.getAvailable(),
-                item.getLandmarkPhone(),
-                item.getLat(),
-                item.getLon(),
-                item.getImage()
-        ));
-        return responseDTO;
-    }
-
-    private LandmarkSaveResponseDTO update(LandmarkSaveRequestDTO requestDTO) {
-        try {
+        private LandmarkSaveResponseDTO create (LandmarkSaveRequestDTO requestDTO){
             final LandmarkFullModel item = template.queryForObject(
                     // language=PostgreSQL
                     """
-                            UPDATE landmarks SET  name = :name, city = :city, image = :image, landmark_address = :landmarkAddress, 
-                            undergrounds = :undergrounds, landmark_description = :landmarkDescription, 
-                                landmark_web_site = :landmarkWebSite, landmark_phone = :landmarkPhone, lat = :lat, lon = :lon
-                                WHERE id = :id and removed = FALSE
-                            RETURNING id, name, city, image, landmark_address, undergrounds, landmark_description, 
-                                landmark_web_site, landmark_phone, lat, lon
+                            INSERT INTO landmarks (name, city, landmark_address, undergrounds, landmark_description, 
+                                landmark_web_site, landmark_phone, image, open, close, lat, lon)  
+                                VALUES (:name, :city, :landmarkAddress, :undergrounds, 
+                                :landmarkDescription, :landmarkWebSite, :landmarkPhone, :image, :open::time with time zone AT TIME ZONE  'UTC',
+                                :close::time with time zone AT TIME ZONE  'UTC', :lat, :lon)
+                            RETURNING id, name, city, landmark_address, undergrounds, landmark_description, 
+                                landmark_web_site, landmark_phone, image, open, close, CURRENT_TIME BETWEEN open AND close AS available, lat, lon
                             """,
-                    Map.of(
-                            "id", requestDTO.getId(),
-                            "name", requestDTO.getName(),
-                            "city", requestDTO.getCity(),
-                            "landmarkDescription", requestDTO.getLandmarkDescription(),
-                            "landmarkPhone", requestDTO.getLandmarkPhone(),
-                            "image", requestDTO.getImage() == null ? defaultImage : requestDTO.getImage(),
-                            "open", "08:00 +03:00",
-                            "close", "20:00 +03:00",
-                            "lat", requestDTO.getLat(),
-                            "lon", requestDTO.getLon()
+                    Map.ofEntries(
+                            Map.entry("name", requestDTO.getName()),
+                            Map.entry("city", requestDTO.getCity()),
+                            Map.entry("landmarkAddress", requestDTO.getLandmarkAddress()),
+                            Map.entry("undergrounds", requestDTO.getUndergrounds()),
+                            Map.entry("landmarkDescription", requestDTO.getLandmarkDescription()),
+                            Map.entry("landmarkWebSite", requestDTO.getLandmarkWebSite()),
+                            Map.entry("landmarkPhone", requestDTO.getLandmarkPhone()),
+                            Map.entry("image", requestDTO.getImage() == null ? defaultImage : requestDTO.getImage()),
+                            Map.entry("open", requestDTO.getOpen()),
+                            Map.entry("close", requestDTO.getClose()),
+                            Map.entry("lat", requestDTO.getLon()),
+                            Map.entry("lon", requestDTO.getLat())
                     ),
                     landmarkFullMapper
 
@@ -247,45 +206,92 @@ public class LandmarkManager {
                     item.getImage()
             ));
             return responseDTO;
-        } catch (EmptyResultDataAccessException e) {
-            throw new LandmarkNotFoundException();
         }
 
-    }
+        private LandmarkSaveResponseDTO update (LandmarkSaveRequestDTO requestDTO){
+            try {
+                final LandmarkFullModel item = template.queryForObject(
+                        // language=PostgreSQL
+                        """
+                                UPDATE landmarks SET  name = :name, city = :city, image = :image, landmark_address = :landmarkAddress, 
+                                undergrounds = :undergrounds, landmark_description = :landmarkDescription, 
+                                    landmark_web_site = :landmarkWebSite, landmark_phone = :landmarkPhone, lat = :lat, lon = :lon
+                                    WHERE id = :id and removed = FALSE
+                                RETURNING id, name, city, image, landmark_address, undergrounds, landmark_description, 
+                                    landmark_web_site, landmark_phone, lat, lon
+                                """,
+                        Map.of(
+                                "id", requestDTO.getId(),
+                                "name", requestDTO.getName(),
+                                "city", requestDTO.getCity(),
+                                "landmarkDescription", requestDTO.getLandmarkDescription(),
+                                "landmarkPhone", requestDTO.getLandmarkPhone(),
+                                "image", requestDTO.getImage() == null ? defaultImage : requestDTO.getImage(),
+                                "open", "08:00 +03:00",
+                                "close", "20:00 +03:00",
+                                "lat", requestDTO.getLat(),
+                                "lon", requestDTO.getLon()
+                        ),
+                        landmarkFullMapper
 
-    private String getImage(String image) {
-        return image == null ? defaultImage : image;
-    }
+                );
+
+                final LandmarkSaveResponseDTO responseDTO = new LandmarkSaveResponseDTO(new LandmarkSaveResponseDTO.Landmark(
+                        item.getId(),
+                        item.getName(),
+                        item.getCity(),
+                        item.getLandmarkAddress(),
+                        item.getUndergrounds(),
+                        item.getLandmarkDescription(),
+                        item.getLandmarkWebSite(),
+                        item.getOpen(),
+                        item.getClose(),
+                        item.getAvailable(),
+                        item.getLandmarkPhone(),
+                        item.getLat(),
+                        item.getLon(),
+                        item.getImage()
+                ));
+                return responseDTO;
+            } catch (EmptyResultDataAccessException e) {
+                throw new LandmarkNotFoundException();
+            }
+
+        }
+
+        private String getImage (String image){
+            return image == null ? defaultImage : image;
+        }
 
 
-    public void removeById(long id) {
-        final int affected = template.update(
-                // language=PostgreSQL
-                """
-                        UPDATE landmarks SET removed = TRUE WHERE id = :id
-                        """,
-                Map.of("id", id)
-        );
-        if (affected == 0) {
-            throw new LandmarkNotFoundException("landmark c таким id(" + id + ") не найден.");
+        public void removeById ( long id){
+            final int affected = template.update(
+                    // language=PostgreSQL
+                    """
+                            UPDATE landmarks SET removed = TRUE WHERE id = :id
+                            """,
+                    Map.of("id", id)
+            );
+            if (affected == 0) {
+                throw new LandmarkNotFoundException("landmark c таким id(" + id + ") не найден.");
+            }
+        }
+
+        public void restoreById ( long id){
+            final int affected = template.update(
+                    // language=PostgreSQL
+                    """
+                            UPDATE landmarks SET removed = FALSE WHERE id = :id
+                            """,
+                    Map.of("id", id)
+            );
+            if (affected == 0) {
+                throw new LandmarkNotFoundException("landmark c таким id(" + id + ") не найден.");
+            }
+        }
+
+        private int distanceFormula ( double lat, double lon, double lat2, double lon2){
+            return (int) (1111.2 * Math.sqrt((lon - lon2) * (lon - lon2) + (lat - lat2) * Math.cos(Math.PI * lon / 180) * (lat - lat2) * Math.cos(Math.PI * lon / 180)));
         }
     }
-
-    public void restoreById(long id) {
-        final int affected = template.update(
-                // language=PostgreSQL
-                """
-                        UPDATE landmarks SET removed = FALSE WHERE id = :id
-                        """,
-                Map.of("id", id)
-        );
-        if (affected == 0) {
-            throw new LandmarkNotFoundException("landmark c таким id(" + id + ") не найден.");
-        }
-    }
-
-    private int distanceFormula(double lat, double lon, double lat2, double lon2) {
-        return (int) (1111.2 * Math.sqrt((lon - lon2) * (lon - lon2) + (lat - lat2) * Math.cos(Math.PI * lon / 180) * (lat - lat2) * Math.cos(Math.PI * lon / 180)));
-    }
-}
 
